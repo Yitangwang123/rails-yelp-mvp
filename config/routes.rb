8a4aa3afdbac9bf42d97af087756dd1bcb9ae520
Yitang Wang <yitangwang0@gmail.com>
2021-07-21 15:21:06 +0800

    backend mvc pattern'

--- config/routes.rb
+++ config/routes.rb
@@ -1,3 +1,18 @@
 Rails.application.routes.draw do
-  # For details on the DSL available within this file, see https://guides.rubyonrails.org/routing.html
+
+  # # For details on the DSL available within this file, see https://guides.rubyonrails.org/routing.html
+  # get '/restaurants', to: 'restaurants#index', as: :restaurants
+
+  # # Add tasks
+  # # show the form
+  # get '/restaurants/new', to: 'restaurants#new', as: :new_restaurant
+  # # end the content and add a task
+  # post '/restaurants', to: 'restaurants#create'
+
+  # # See details about one task
+  # get '/restaurants/:id', to: 'restaurants#show', as: :restaurant
+
+  resources :restaurants, only: [:index, :show, :new, :create] do
+    resources :reviews, only: :create
+  end
 end
--- db/seeds.rb
+++ db/seeds.rb
@@ -5,3 +5,26 @@
 #
 #   movies = Movie.create([{ name: 'Star Wars' }, { name: 'Lord of the Rings' }])
 #   Character.create(name: 'Luke', movie: movies.first)
+require 'faker'
+
+puts 'Creating 5 fake restaurants...'
+Restaurant.destroy_all
+5.times do
+  restaurant = Restaurant.new(
+    name: Faker::Restaurant.name,
+    address: Faker::Address.full_address,
+    phone_number: Faker::PhoneNumber,
+    category: ['chinese', 'italian', 'japanese', 'french', 'belgian'].sample
+  )
+  restaurant.save!
+
+  3.times do
+    review = Review.new(
+      content: Faker::Book.title,
+      rating: (0..5).to_a.sample,
+      restaurant: restaurant
+    )
+    review.save!
+  end
+end
+puts 'Finished!'
